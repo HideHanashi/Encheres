0ae2cffa9bef119b7712c43a7f5573e08a4c7b38
pELouanp <123365542+pELouanp@users.noreply.github.com>
2023-09-20 15:30:54 +0200

    test profil

--- src/main/java/fr/eni/encheres/ihm/AfficherProfilServlet.java
+++ src/main/java/fr/eni/encheres/ihm/AfficherProfilServlet.java
@@ -8,31 +8,31 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/autre-profil")
+@WebServlet("/monprofil")
 public class AfficherProfilServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		   
-		   String pseudo = request.getParameter("pseudo");
-		   String nom = request.getParameter("nom");
-		   String prenom = request.getParameter("prenom");
-		   String email = request.getParameter("email");
-		   String telephone = request.getParameter("telephone");
-		   String rue = request.getParameter("rue");
-		   String code_postal = request.getParameter("code_postal");
-		   String ville = request.getParameter("ville");
-		   
-		   request.setAttribute("pseudo", pseudo);
-	       request.setAttribute("nom", nom);
-	       request.setAttribute("prenom", prenom);
-	       request.setAttribute("email", email);
-	       request.setAttribute("telephone", telephone);
-	       request.setAttribute("rue", rue);
-	       request.setAttribute("code_postal", code_postal);
-	       request.setAttribute("ville", ville);
-	      
-	       request.getRequestDispatcher("/WEB-INF/pages/autre-profil.jsp").forward(request, response);
-	   }
-}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
+		String pseudo = request.getParameter("pseudo");
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
+		String email = request.getParameter("email");
+		String telephone = request.getParameter("telephone");
+		String rue = request.getParameter("rue");
+		String code_postal = request.getParameter("code_postal");
+		String ville = request.getParameter("ville");
+
+		request.setAttribute("pseudo", pseudo);
+		request.setAttribute("nom", nom);
+		request.setAttribute("prenom", prenom);
+		request.setAttribute("email", email);
+		request.setAttribute("telephone", telephone);
+		request.setAttribute("rue", rue);
+		request.setAttribute("code_postal", code_postal);
+		request.setAttribute("ville", ville);
+
+		request.getRequestDispatcher("/WEB-INF/pages/mon-profil.jsp").forward(request, response);
+	}
+}
