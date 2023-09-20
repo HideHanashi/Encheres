5672d747b2a232ad70f1ee9c7cfc4407932de08f
pELouanp <123365542+pELouanp@users.noreply.github.com>
2023-09-20 12:07:07 +0200

    encore error

--- src/main/java/fr/eni/encheres/ihm/ModifierProfilServlet.java
+++ src/main/java/fr/eni/encheres/ihm/ModifierProfilServlet.java
@@ -13,19 +13,21 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/modifier-mon-profil")
 public class ModifierProfilServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-				try {
-					int noUtilisateur = Integer.parseInt(request.getParameter("noUtilisateur"));
-					Utilisateur utilisateur =  UtilisateursManager.getInstance().recupUtilisateur(noUtilisateur);
-					request.setAttribute("utilisateur", utilisateur);
-					request.getRequestDispatcher("/WEB-INF/pages/modifier-mon-profil.jsp").forward(request, response);
-				} catch (Exception e) {
-					response.sendError(404);
-				}				
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			int noUtilisateur = Integer.parseInt(request.getParameter("noUtilisateur"));
+			Utilisateur utilisateur = UtilisateursManager.getInstance().recupUtilisateur(noUtilisateur);
+			request.setAttribute("utilisateur", utilisateur);
+			request.getRequestDispatcher("/WEB-INF/pages/modifier-mon-profil.jsp").forward(request, response);
+		} catch (Exception e) {
+			response.sendError(404);
+		}
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		try {
 			int noUtilisateur = Integer.parseInt(request.getParameter("noUtilisateur"));
 			String pseudo = request.getParameter("pseudo");
@@ -35,11 +37,12 @@ public class ModifierProfilServlet extends HttpServlet {
 			String telephone = request.getParameter("telephone");
 			String rue = request.getParameter("rue");
 			String codePostal = request.getParameter("codePostal");
-			String ville = request.getParameter("ville");		
-			Utilisateur utilisateur = new Utilisateur(noUtilisateur, pseudo, nom, prenom, email, telephone, rue, codePostal, ville);
+			String ville = request.getParameter("ville");
+			Utilisateur utilisateur = new Utilisateur(noUtilisateur, pseudo, nom, prenom, email, telephone, rue,
+					codePostal, ville);
 			UtilisateursManager.getInstance().modifyUtilisateur(utilisateur);
-			response.sendRedirect( request.getContextPath() +"/mon-profil");
-		}catch (Exception e) {
+			response.sendRedirect(request.getContextPath() + "/mon-profil");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
