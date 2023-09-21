4f305084f56aba46323ff0759a13707bf5e7e4a1
Jules <jules.motreff@gmail.com>
2023-09-21 15:26:56 +0200

    Servlet v8

--- src/main/java/fr/eni/encheres/bo/Utilisateur.java
+++ src/main/java/fr/eni/encheres/bo/Utilisateur.java
@@ -71,20 +71,6 @@ public class Utilisateur {
 
 	}
 
-
-	public Utilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
-			String rue, String codePostal, String ville) {
-		this.noUtilisateur = noUtilisateur;
-		this.pseudo = pseudo;
-		this.nom = nom;
-		this.prenom = prenom;
-		this.email = email;
-		this.telephone = telephone;
-		this.rue = rue;
-		this.codePostal = codePostal;
-		this.ville = ville;
-	}
-
 	public Utilisateur() {
 		
 	}
