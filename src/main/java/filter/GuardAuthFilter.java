a7af4e3b8a0d63ebb47de97cd74e2eab7c57d58e
pELouanp <123365542+pELouanp@users.noreply.github.com>
2023-12-20 20:16:05 +0100

    Update GuardAuthFilter.java

--- src/main/java/filter/GuardAuthFilter.java
+++ src/main/java/filter/GuardAuthFilter.java
@@ -15,22 +15,31 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
+// Définition d'un filtre Web avec certaines configurations
 @WebFilter(dispatcherTypes = { DispatcherType.REQUEST }, urlPatterns = { "/deconnexion", "/modifiermonprofil",
 		"/supprimermonprofil", "/monprofil", "/nouvelle-vente", "/voirarticles", "/modifiermesarticles", "/articleparticipe" })
 public class GuardAuthFilter extends HttpFilter implements Filter {
 
+	// Numéro de version de la classe (non utilisé dans ce code)
 	private static final long serialVersionUID = 1L;
 
+	// Méthode de filtrage des requêtes
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 
+		// Conversion des objets de requête et de session pour les utiliser spécifiquement pour les requêtes HTTP
 		HttpServletRequest rq = (HttpServletRequest) request;
 		HttpSession session = rq.getSession();
+
+		// Vérifie si l'utilisateur est connecté en vérifiant la présence d'un attribut "user" dans la session
 		if (session.getAttribute("user") == null) {
+			// Si l'utilisateur n'est pas connecté, redirige vers la page de connexion
 			HttpServletResponse rs = (HttpServletResponse) response;
 			rs.sendRedirect(rq.getContextPath() + "/connexion");
-			return;
+			return; // Arrête le traitement du filtre
 		}
+
+		// Si l'utilisateur est connecté, permet à la requête de continuer son traitement normal
 		chain.doFilter(request, response);
 	}
 }
