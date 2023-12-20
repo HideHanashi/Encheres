b58bd35da980ad548b3f0ba28be6a30f03f11fa0
pELouanp <123365542+pELouanp@users.noreply.github.com>
2023-12-20 20:25:42 +0100

    Update UtilisateursManager.java

--- src/main/java/fr/eni/encheres/bll/UtilisateursManager.java
+++ src/main/java/fr/eni/encheres/bll/UtilisateursManager.java
@@ -1,4 +1,4 @@
- package fr.eni.encheres.bll;
+package fr.eni.encheres.bll;
 
 import java.util.List;
 import java.util.Random;
@@ -14,71 +14,75 @@ import fr.eni.encheres.helper.PasswordEncoder;
 
 public class UtilisateursManager {
 
-	// DÉBUT SINGLETON
+	// DÉBUT DU SINGLETON
 
 	private static UtilisateursManager instance;
 
 	private UtilisateursManager() {
 	}
 
+	// Méthode pour obtenir l'instance unique de la classe
 	public static UtilisateursManager getInstance() {
 		if (instance == null)
 			instance = new UtilisateursManager();
 		return instance;
 	}
 
-	// FIN SINGLETON
+	// FIN DU SINGLETON
 
-	// DÉBUT LA LOGIQUE MÉTIER
+	// DÉBUT DE LA LOGIQUE MÉTIER
 
 	// BLL UTILISATEUR
 
-	private UtilisateurDao utilisateurDao = DaoFactory.getUtilisateurDao();
-
+	// Récupère un utilisateur par son numéro d'identification
 	public Utilisateur recupUtilisateur(int noUtilisateur) {
 		return utilisateurDao.findOne(noUtilisateur);
 	}
 
+	// Récupère tous les utilisateurs
 	public List<Utilisateur> recupTousLesUtilisateurs() {
 		return utilisateurDao.findAll();
 	}
 
+	// Générateur de nombres aléatoires
 	private Random rd = new Random();
 
-//	public List<Utilisateur> searchAllUtilisateur() {
-//		return utilisateurDao.findAll();
-//	}
-
+	// Ajoute un nouvel utilisateur
 	public void addUtilisateur(Utilisateur user) throws JDBCException {
 		utilisateurDao.save(user);
 	}
 
+	// Modifie les informations d'un utilisateur
 	public void modifyUtilisateur(Utilisateur utilisateur) throws BLLException {
 		utilisateurDao.modify(utilisateur);
 	}
 
+	// Supprime un utilisateur par son numéro d'identification
 	public void removeUtilisateur(int noUtilisateur) {
 		utilisateurDao.remove(noUtilisateur);
-		;
 	}
 
+	// Recherche un utilisateur par son pseudo ou son email
 	public Utilisateur searchUtilisateur(String query) {
 		return utilisateurDao.findByUsername(query);
 	}
 	
+	// Modifie le crédit d'un utilisateur
 	public void modifyCredit(Utilisateur user) {
 		utilisateurDao.modifyCredit(user);
 	}
 
 	// BLL AUTRES
 
+	// Méthode d'inscription d'un nouvel utilisateur
 	public void inscription(Utilisateur utilisateur) throws JDBCException, BLLException {
 		checkFields(utilisateur);
-		utilisateur.setMotDePasse(PasswordEncoder.hashPassword(utilisateur.getMotDePasse() // PASSWORD NON HACHER
-		));
+		// Hachage du mot de passe avant de le stocker
+		utilisateur.setMotDePasse(PasswordEncoder.hashPassword(utilisateur.getMotDePasse()));
 		utilisateurDao.save(utilisateur);
 	}
 
+	// Vérifie les champs obligatoires lors de l'inscription
 	private void checkFields(Utilisateur utilisateur) throws BLLException {
 		if (utilisateur == null)
 			throw new BLLException("User est null");
@@ -88,14 +92,14 @@ public class UtilisateursManager {
 		if (utilisateur.getEmail().isBlank())
 			throw new BLLException("L'e-mail est obligatoire !");
 
-		// VÉRIFIE LA SYNTAXE DE L'EMAIL
+		// Vérifie la syntaxe du mot de passe
 		if (utilisateur.getMotDePasse().isBlank())
 			throw new BLLException("Le mot de passe est obligatoire !");
 		if (utilisateur.getMotDePasse().length() < 8 || utilisateur.getMotDePasse().length() > 35)
-			throw new BLLException("La taille du mot de passe doit etre entre 8 et 35 caractères !");
-
+			throw new BLLException("La taille du mot de passe doit être entre 8 et 35 caractères !");
 	}
 
+	// Méthode de connexion d'un utilisateur
 	public Utilisateur login(String email, String password) {
 		Utilisateur utilisateur = utilisateurDao.findByEmail(email);
 		if (utilisateur != null && utilisateur.getEmail().equals(email)
@@ -105,6 +109,7 @@ public class UtilisateursManager {
 		return null;
 	}
 
+	// Récupère les informations d'un utilisateur par son numéro d'identification
 	public Utilisateur profil(int noUtilisateur) {
 		Utilisateur utilisateur = utilisateurDao.findOne(noUtilisateur);
 		if (utilisateur != null && utilisateur.getNoUtilisateur() == (noUtilisateur)) {
@@ -113,47 +118,49 @@ public class UtilisateursManager {
 		return null;
 	}
 
+	// Vérifie l'existence d'un email et génère un code pour la réinitialisation du mot de passe
 	public ForgetPassword checkEmail(String email) throws BLLException {
 
 		Utilisateur utilisateur = utilisateurDao.findByEmail(email);
 		if (utilisateur == null)
 			throw new BLLException("Erreur: l'email n'existe pas");
 
-		// GÉNÉRE LE CODE
+		// Génère le code
 		String code = rd.nextLong(1, 9999999999L) + "";
 		ForgetPassword fp = new ForgetPassword(code, utilisateur);
 
-		// SAVE
+		// Enregistre le code
 		DaoFactory.getForgetPasswordDao().save(fp);
 
-		// ENVOIE LE MAIL
-		// PAR SMS VIA API
+		// Envoie le code par SMS via une API
 		System.out.println(code);
 
 		return fp;
 	}
 
+	// Réinitialise le mot de passe avec un nouveau mot de passe
 	public void resetPassword(String email, String code, String newPassword) throws BLLException {
 
 		ForgetPassword fp = DaoFactory.getForgetPasswordDao().resetPassword(email);
 
 		if (!fp.getCode().equals(code))
-			throw new BLLException("Le code est érroné !");
+			throw new BLLException("Le code est erroné !");
 
 		Utilisateur utilisateur = fp.getUser();
 
 		utilisateur.setMotDePasse(PasswordEncoder.hashPassword(newPassword));
 
 		utilisateurDao.modifyPassword(utilisateur);
-
 	}
 
 	// BLL ADRESSE
 
+	// Modifie l'adresse d'un utilisateur
 	public void modifyAdresse(Retrait adresse) {
 		utilisateurDao.modify(adresse);
 	}
 
+	// Supprime un utilisateur par son numéro d'identification
 	public void supprimerUtilisateur(int noUtilisateur) {
 		utilisateurDao.remove(noUtilisateur);
 	}
